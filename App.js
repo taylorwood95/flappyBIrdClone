4e9c18ccc41ab27c382c70ef55dddfe385303496
taylorwood95 <115211674+taylorwood95@users.noreply.github.com>
2023-07-10 15:08:18 +0200

    square displaying

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import { GameEngine } from 'react-native-game-engine';
+import entities from './entities';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1}}>
+      <GameEngine
+      entities={entities()}
+      style={{
+        postion: 'absolute',top: 0,right: 0,bottom: 0,left: 0}}
+      >
+
+      </GameEngine>
+      <StatusBar style="auto" hidden={true}/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
